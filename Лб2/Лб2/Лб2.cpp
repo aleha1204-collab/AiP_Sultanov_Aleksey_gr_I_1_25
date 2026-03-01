a39d0ca7cd0f604c19f514a2cc863cf9cbfe58a5
aleha1204-collab <aleha1204@gmail.com>
2026-03-01 13:19:27 +0300

    Лб2

---
+++ Лб2/Лб2/Лб2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    cout << "Введи число: ";
+    double x;
+    cin >> x;
+    float a = x;
+    double b = x;
+    double raz = b - a;
+    cout << fixed << setprecision(15);
+    cout << "\nFloat:  " << a << endl;
+    cout << "Double: " << b << endl;
+    cout << "Разница: " << raz << endl;
+    system("pause");
+    return 0;
+}
